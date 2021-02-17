3610fad1ad8cb172e48a1f1dfef5fea9fff3609a
JoonOh-Lee <wnsxoddl2020@naver.com>
2021-02-17 15:24:54 +0900

    ㅇㅇㅇ
    ㅇㅇㅇㅁㄴㅇ

--- Myhome_project/src/com/test/user/MypageInfo.java
+++ Myhome_project/src/com/test/user/MypageInfo.java
@@ -15,7 +15,7 @@ public class MypageInfo extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/user/mypage-info.jsp");
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/user/mypage/mypage-info.jsp");
 		dispatcher.forward(req, resp);
 
 	}
--- Myhome_project/src/com/test/user/MypageLike.java
+++ Myhome_project/src/com/test/user/MypageLike.java
@@ -1,5 +1,23 @@
 package com.test.user;
 
-public class MypageLike {
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/user/mypagelike.do")
+public class MypageLike extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/user/mypage/mypage-like.jsp");
+		dispatcher.forward(req, resp);
+
+	}
 
 }
