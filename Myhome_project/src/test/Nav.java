03a64a7bdc3feaf09540960b94d0c52430755e30
jennyjipark <jennyjipark@gmail.com>
2021-02-17 21:31:25 +0900

    1

---
+++ Myhome_project/src/test/Nav.java
@@ -0,0 +1,22 @@
+package test;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin/navboard.do")
+public class Nav extends HttpServlet{
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/admin/navboard.jsp");
+		dispatcher.forward(request, response);
+		
+	}
+}
