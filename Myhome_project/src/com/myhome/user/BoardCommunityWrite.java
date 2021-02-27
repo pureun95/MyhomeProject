7ae6e47cf8ee051d370b4a1fa1369ff26033d04d
pureun95 <77381499+pureun95@users.noreply.github.com>
2021-02-27 23:42:37 +0900

    Revert "Revert "풀리퀘보냅니다!""
    This reverts commit d624d520c611605a9a54d08c5b5ce9830d1f8745.

--- Myhome_project/src/com/myhome/user/BoardCommunityWrite.java
+++ Myhome_project/src/com/myhome/user/BoardCommunityWrite.java
@@ -12,38 +12,34 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @WebServlet("/Myhome/user/boardcommunitywrite.do")
-public class BoardCommunityWrite extends HttpServlet{
+public class BoardCommunityWrite extends HttpServlet {
 
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		HttpSession session = request.getSession();
-		
-		//로그인 연동 완료되면 수정하기*****
+
+		// 로그인 연동 완료되면 수정하기*****
 		if (session.getAttribute("name") == null) {
-			
+
 			PrintWriter writer = response.getWriter();
-			
+
 			writer.print("<html><body>");
 			writer.print("<script>");
 			writer.print("alert('로그인이 필요합니다.');");
 			writer.print("location.href='/Myhome_project/Myhome/user/boardcommunitylist.do';");
 			writer.print("</script>");
 			writer.print("</body></html>");
-			
+
 			writer.close();
-			
+
 			return;
 
 		}
-		
 
-		
-		
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/user/board-community-write.jsp");
 		dispatcher.forward(request, response);
-		
+
 	}
 }
-
-
